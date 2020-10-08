e886d7a3d4853d61a1f2857d6281880fcfa06d11
AlisonPinheiro <alisonpinheiro17@gmail.com>
2020-10-08 13:24:19 -0300

    styles from icon box component and finish services page

--- src/Data/servicesData.js
+++ src/Data/servicesData.js
@@ -1,13 +1,40 @@
-export const servicesObj = {
-  lightBackground: true,
-  lightText: false,
-  lightTextDescription: true,
-  topLine: "Exclusive Acess",
-  headLine: "Unlimted transactions with zero",
-  description:
-    "Get Acess to our exclusive diamond card tha allows you to send unlimited transactions withot gettint chaged any fees",
-  buttonLabel: "Get Started",
-  imgStart: "",
-  img: "images/svg-1.svg",
-  alt: "Credit Card",
+export const ourServices = {
+  light: true,
+  content: {
+    title: "Nossa Estrutura",
+    text:
+      "Há anos atuando no mercado brasileiro, a empresa é especializada em mudanças residenciais e comerciais, além de prestar serviços complementares. Possui uma estrutura própria composta por:",
+  },
+};
+
+export const servicesIcons = [
+  {
+    title: "Veículos próprios rastreados",
+    icon: "/images/carro.png",
+  },
+  {
+    title: "Embalagens de diversos tipos",
+    icon: "/images/embalagem.png",
+  },
+  {
+    title: "Funcionários capacitados",
+    icon: "/images/team.png",
+  },
+  {
+    title: "Depósitos para armazenagem",
+    icon: "/images/build.png",
+  },
+  {
+    title: "Equipamentos",
+    icon: "/images/tool.png",
+  },
+];
+
+export const ourSpecialty = {
+  light: true,
+  content: {
+    title: "Especialidades",
+    text:
+      "Somos especializados em mudanças residenciais e corporativas, além de oferecer serviços complementares, como guarda-móveis, içamentos e transportes especiais. Veja o que realizamos:",
+  },
 };
--- src/Pages/Services/index.js
+++ src/Pages/Services/index.js
@@ -1,19 +1,63 @@
 import React from "react";
-import Pricing from "../../Components/Pricing";
-import Section from "../../Components/Section";
-import { servicesObj } from "../../Data/servicesData";
-import { homeObjeOne } from "../../Data/homeData";
+import {
+  ourServices,
+  servicesIcons,
+  ourSpecialty,
+  specialtyIcons,
+} from "../../Data/servicesData";
+import TitlePage from "../../Components/TitlePage";
+import CallActionMiddle from "../../Components/CallActionMiddle";
+import TextandImg from "../../Components/ContentBox/TextandImage";
+import IconsBox from "../../Components/ContentBox/IconsBox";
 
 import "./Services.css";
 
 function Services() {
   return (
-    <>
-      <h1> Services</h1>
-      <Section {...servicesObj} />
-      <Pricing />
-      <Section {...homeObjeOne} />
-    </>
+    <div className="services">
+      <TitlePage
+        title="Nosso Serviços"
+        subTitle="conheça os serviços da Transferilar"
+      />
+      <TextandImg data={ourServices} />
+      <IconsBox data={servicesIcons} />
+      <CallActionMiddle />
+      <TextandImg data={ourSpecialty} />
+      <section className="content-box light icons">
+        <div className="container">
+          <div className="services-list">
+            <ul className="content-list">
+              <li className="list-item">
+                <h3>Mudanças nacionais e internacionais</h3>
+              </li>
+              <li className="list-item">
+                <h3>Embalagens de móveis e itens em geral</h3>
+              </li>
+              <li className="list-item">
+                <h3>Desmontagem e montagem</h3>
+              </li>
+              <li className="list-item">
+                <h3>Transporte de pianos e cofres</h3>
+              </li>
+            </ul>
+            <ul className="content-list">
+              <li className="list-item">
+                <h3>Içamentos</h3>
+              </li>
+              <li className="list-item">
+                <h3>Transporte de veículos</h3>
+              </li>
+              <li className="list-item">
+                <h3>Transporte de equipamentos sensíveis</h3>
+              </li>
+              <li className="list-item">
+                <h3>Remanejamento interno de setor</h3>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 }
 
